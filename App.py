9a0e0a0ab7f9ac1671a1059a5d1530d9f16d7741
AlexHCGit <167798456+AlexHCGit@users.noreply.github.com>
2024-05-04 14:24:27 +0200

    Add files via upload

---
+++ App.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import streamlit as st
+import openai
+import langchain
+import tiktoken
+from dotenv import load_dotenv, find_dotenv
+import os
+from langchain import PromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.chains.summarize import load_summarize_chain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.docstore.document import Document
+
+
+# In[ ]:
+
+
+load_dotenv(find_dotenv(), override=True)
+
+
+# In[ ]:
+
+
+def crear_resumen(txt):
+    llm = ChatOpenAI(temperature=0.5, model_name='gpt-4')
+    
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=20000, chunk_overlap=500)
+    chunks=text_splitter.create_documents([texto])
+    
+    map_prompt='''
+    Escriba un resumen conciso y breve del siguiente texto:
+    Que el resumen esté en español.
+    TEXTO: {text}
+    '''
+    map_prompt_template = PromptTemplate(
+        input_variables=['text'],
+        template=map_prompt
+    )
+    
+    prompt_combinado='''
+    Escriba un resumen conciso del siguiente texto que abarque los puntos claves.
+    Añade un título al resumen.
+    Comience su resumen con un PÁRRAFO DE INTRODUCCIÓN que ofrezca una visión general del tema SEGUIDO
+    por los PUNTOS tipo "BULLET Point" si es posible Y termine el resumen con una CONCLUSIÓN.
+    Texto: {text}
+    '''
+    prompt_template_combinado= PromptTemplate(
+        input_variables=['text'],
+        template=prompt_combinado
+    )
+    
+    cadena_resumen = load_summarize_chain(
+    llm=llm,
+    chain_type='map_reduce',
+    map_prompt = map_prompt_template,
+    combine_prompt=prompt_template_combinado,
+    verbose=False
+    )
+    
+    return cadena_resumen.run(chunks)
+
+# Título App en Web
+st.set_page_config(page_title='App Resumen Texto')
+st.title('App Resumen Texto')
+
+# Introducción Texto
+texto = st.text_area('Introduce texto a resumir:', '', height=200)
+
+# Aceptar el texto introducido por el usuario para el resumen
+result=[]
+with st.form('summarize_form', clear_on_submit=True):
+    submitted = st.form_submit_button('Submit')
+    if submitted:
+        with st.spinner('Resumiendo...'):
+            resumen = crear_resumen(texto)
+            result.append(resumen)
+
+if len(result):
+    st.info(resumen)
+            
+    
+
