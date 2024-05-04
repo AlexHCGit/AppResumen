56b393a153a6f46aba8163711fe8ad79728f8ce8
AlexHCGit <167798456+AlexHCGit@users.noreply.github.com>
2024-05-04 14:53:26 +0200

    Add files via upload

--- App.py
+++ App.py
@@ -5,11 +5,9 @@
 
 
 import streamlit as st
-import openai
 import langchain
 import tiktoken
 from dotenv import load_dotenv, find_dotenv
-import os
 from langchain import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.chains.summarize import load_summarize_chain
@@ -20,15 +18,8 @@ from langchain.docstore.document import Document
 # In[ ]:
 
 
-#load_dotenv(find_dotenv(), override=True)
-api_key="sk-proj-IYP3uNpGeC8Xq9I5ein6T3BlbkFJCT0bfEE2zKFxE9qCTlAU"
-
-
-# In[ ]:
-
-
 def crear_resumen(txt):
-    llm = ChatOpenAI(temperature=0.5, model_name='gpt-4', openai_api_key=api_key)
+    llm = ChatOpenAI(temperature=0.5, model_name='gpt-4', openai_api_key=openai_api_key)
     
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=20000, chunk_overlap=500)
     chunks=text_splitter.create_documents([texto])
@@ -75,8 +66,9 @@ texto = st.text_area('Introduce texto a resumir:', '', height=200)
 # Aceptar el texto introducido por el usuario para el resumen
 result=[]
 with st.form('summarize_form', clear_on_submit=True):
+    openai_api_key = st.text_input('OpenAI API Key', type = 'password', disabled=not texto)
     submitted = st.form_submit_button('Submit')
-    if submitted:
+    if submitted and openai_api_key.startswith('sk-'):
         with st.spinner('Resumiendo...'):
             resumen = crear_resumen(texto)
             result.append(resumen)
